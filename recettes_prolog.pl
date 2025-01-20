43522d899b71cb2139c1eac0ac6d32845ea04a2f
Tufan95 <kabalitufan24@gmail.com>
2025-01-20 10:40:29 +0100

    Création fichier Prolog

---
+++ recettes_prolog.pl
@@ -0,0 +1,18 @@
+
+% Définition des recettes et de leurs ingrédients
+recette(pizza, [farine, eau, sel, levure, tomate, fromage]).
+recette(salade, [laitue, tomate, concombre, vinaigre, huile]).
+recette(pates_carbonara, [pates, creme, lardons, fromage, sel, poivre]).
+recette(omelette, [oeufs, sel, poivre, fromage, herbes]).
+recette(sandwich_jambon_beurre, [pain, beurre, jambon, salade]).
+
+% Règle pour vérifier si tous les ingrÃ©dients d'une recette sont
+% disponibles
+recette_possible(Recette, IngredientsDisponibles) :-
+    recette(Recette, IngredientsNecessaires),
+    subset(IngredientsNecessaires, IngredientsDisponibles).
+
+% Sous-ensemble : vérifie si tous les éléments d'une liste sont dans
+% une autre
+subset([], _).
+subset([X|XS], L) :- member(X, L), subset(XS, L).
